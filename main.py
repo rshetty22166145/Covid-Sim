0f34f88569a399a369a0214a78ef2f7c9f457364
Alex Panas <alexpanas@Alexs-MacBook-Pro.local>
2021-11-08 16:51:56 -0500

    Add pan and zoom

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 """
 Main file where the program will start
+
+class App:
+    Overhead manager for the app window and app navigation
+    - Contains user's screen info
+    - Contains pygame window
+    - Runs main menu navigation including sim creation form
+
+class Sim:
+    Manager for the simulation
+    - Contains city instance
+    - Contains people instances
+    - Contains main loop for running sim
+    - Runs pygame and all events
+    - Potentially has a computational thread to update the sim based on speed rate
+    - Keeps track of and saves sim data
+
+class SimRenderer:
+    Takes in info from sim and renders frame
+
+class Camera:
+    Dataclass to store cam info
+
+class Person:
+    Person info
+    - Contains personality traits
+    - Contains age, gender, and other characteristics
+    - Stores and manages decision info
+    - Contains infection info
+    - Stores location info
+
+class City:
+    City Info
+    - Contains city grid dimensions
+    - Contains buildings instances
+    - Contains methods to be used by Person class for navigation (ie travel_to(target, current_loc))
+
+class Building
+    Building info
+    - Contains grid coordinate of building
+    - Contains building characteristics info
+
 """
 
-print("Hello World")
+import Sim
+
+Sim.SimManager().start()
+
